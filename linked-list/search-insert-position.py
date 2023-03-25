6740f1f1300ca841feb823f42a5e40ed4b77963e
akiyomov <asadbey0331@gmail.com>
2023-03-25 15:55:03 +0900

    search-insert

---
+++ linked-list/search-insert-position.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if nums == []:
+            return 0
+        mid  = (len(nums))//2
+        
+        if target == nums[mid]:
+            return mid
+        if target > nums[mid]:
+            return mid+ 1 +  self.searchInsert(nums[mid+1:],target)
+        else: 
+            return self.searchInsert(nums[:mid], target)
