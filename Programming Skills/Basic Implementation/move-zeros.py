917815b8df44dcbcb328ce9f9880cd1d078e06b8
akiyomov <114338623+akiyomov@users.noreply.github.com>
2023-05-31 21:16:39 +0900

    MoveZeros

---
+++ Programming Skills/Basic Implementation/move-zeros.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(nums.count(0)):
+            nums.remove(0)
+            nums.append(0)
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = len(nums)
+        for i in reversed(range(l)):
+            if nums[i] == 0:
+                nums[i:] = nums[i+1:]
+                nums.append(0)
