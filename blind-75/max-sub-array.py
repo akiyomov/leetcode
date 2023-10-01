8910a12dd9e25b2a8841b54cbd3f3a9a635ceeb1
akiyomov <asadbey0331@gmail.com>
2023-10-01 22:42:04 +0500

    Max sub sum of array

---
+++ blind-75/max-sub-array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        currSum = 0
+
+        for i in nums:
+            if currSum < 0:
+                currSum = 0
+            currSum += i
+            maxSum = max(currSum, maxSum)
+        return maxSum
